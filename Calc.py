4c4c79b16a0a9b4cbb269f68e1a2804130925980
aryanjain7 <kjain2@babson.edu>
2016-09-01 14:49:50 -0400

    Sixth Commit Calc.py

--- Calc.py
+++ Calc.py
@@ -1,3 +1,5 @@
+#arithmetic operator testing
+
 print(46+37+38)
 
 print(145/12)
@@ -7,3 +9,12 @@ print(7*11)
 print(145%12)
 
 print(12-112)
+
+#age calculation
+
+print('My age is:')
+print(2016-1995)
+
+#exponent test
+
+print(3**10)
