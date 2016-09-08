fb25e9ab99d3ed7bac1f9f2cdfca9086232ac428
aryanjain7 <kjain2@babson.edu>
2016-09-07 21:49:44 -0400

    Homework 1

--- Calc.py
+++ Calc.py
@@ -18,3 +18,25 @@ print(2016-1995)
 #exponent test
 
 print(3**10)
+
+#HOMEWORK
+#1. Volume of a sphere
+r = 5
+vol = (4/3)*3.1415926*(r^3)
+print('The volume of a sphere with radius'+r+'is'+vol)
+
+#2. Bookstores
+cp = 24.95
+discount = 0.4
+ship1 = 3
+ship = 0.75
+n = 60
+tcost = n*(cp*(1-discount)) + ship1 + (n-1)*ship
+
+#3. Workout
+
+
+#4. Average grade
+increase = (89-82)/82
+increase = increase * 100
+print('The average increased by %.2f' % increase)
--- Hello.py
+++ Hello.py
@@ -1,2 +1,22 @@
-print('Hello, World!')
-print('Hey Jude', 'don\'t mkae it bad')
+print('Hello, World!') #single input print command
+print('Hey Jude', 'don\'t mkae it bad') #\' allows the user to print an apostrophe even within string quotes
+print ('The total number of overall medals in Rio 2016 is', 46 + 37 + 38) #multiple input print command
+
+#Ask for user input
+message1 = 'Please enter your first name: '
+first_name = input(message1)
+message2 = 'Please enter your last name: '
+last_name = input(message2)
+#print('Hello,', first_name + ' ' + last_name)
+age = 2016-1995
+
+# '%' provides a placeholder for multiple entries.
+#print('Hello, %s %s. You are now %d years old' % ('Aryan', 'Jain', 21))
+print('Hello, %s %s. You are now %d years old' % (first_name, last_name, age))
+
+'Today is %2d-%02d.' % (9, 6) #%2d adds an extra space before the 9, #02d adds an extra space and a '0' before the 6.
+'Pi equals %.2f.' % 3.1415926 #%.2f limits the float to 2 decimal places
+'Growth rate: %d %%' % 7 #%% allows the user to actually have a '%' in the output.
+
+
+input()
