5c31786529ddeda683f66ade0b0a232e70caeed5
aryanjain7 <kjain2@babson.edu>
2016-10-13 17:15:46 -0400

    Tuples

--- tuples.py
+++ tuples.py
@@ -8,3 +8,15 @@ def sumall(*nums):
 #testing
 print(sumall(1,2,3,4,5))
 print(sumall(1,2,3,4,5,6,7,8,9,10))
+
+def most_frequent(word):
+    from operator import itemgetter
+    d = dict()
+    for c in word:
+        d[c] = d.get(c,0)+1
+    a = list(d.items())
+    for x,y in sorted(a, key=itemgetter(1), reverse=True):
+        print(x, y)
+
+#testing
+most_frequent('bookkeeper')
