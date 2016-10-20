5d8c09e549d1aa2b725860448475643a6257eb05
aryanjain7 <kjain2@babson.edu>
2016-10-20 15:31:10 -0400

    Roster DB Solution

--- roster.py
+++ roster.py
@@ -1,26 +1,35 @@
 import random
+import dbm
+import os
 
-ROSTER = {"Beshansky": 0,
-          "Collins": 0,
-          "Fischer": 1,
-          "Giovanucci": 0,
-          "Jain": 0,
-          "Kim": 0,
-          "Lauture": 0,
-          "Lee": 0,
-          "Maddox": 0,
-          "Martinez": 0,
-          "Mendez": 0,
-          "Oh": 0,
-          "Petrone": 1,
-          "Posada": 0,
-          "Rule": 1,
-          "Schilb": 0,
-          "Tariq": 0,
-          "Wang": 0,
-          "Wolf": 0}
-
-
+def open_db():
+    ROSTER = ("Beshansky",
+            "Collins",
+            "Fischer",
+            "Giovanucci",
+            "Jain",
+            "Kim",
+            "Lauture",
+            "Lee",
+            "Maddox",
+            "Martinez",
+            "Mendez",
+            "Oh",
+            "Petrone",
+            "Posada",
+            "Rule",
+            "Schilb",
+            "Tariq",
+            "Wang",
+            "Wolf")
+    if not os.path.exists('db_roster.dir'):
+        db = dbm.open('db_roster', 'c')
+        for student in ROSTER:
+            db[student]='0'
+        return db
+    else:
+        db = dbm.open('db_roster', 'c')
+        return db
 def call(roster):
     """
     Among the names that are called least times,
@@ -28,10 +37,26 @@ def call(roster):
     roster: a dict of names and integers
     """
     #print(random.sample(roster, 3))
-    d = dict()
-    for key, value in roster.items():
-        if value == min(roster.values()):
-            d[key] = value
-    return random.choice(list(d.keys()))
+    values = [int(x[1]) for x in roster.items()]
+    minimum = min(values)
 
-print(call(ROSTER))
+    temp = dict()
+    for key, value in roster.items():
+        if int(value) == minimum:
+            temp[key] = value
+
+    student = random.choice(list(temp.keys()))
+
+    visits = int(roster[student]) + 1
+
+    roster[student] = str(visits)
+
+    return student
+
+
+# Testing
+
+roster = open_db()
+print(roster.items())       #Check the current number of visits of each student
+print(call(roster))         #Call a random student who has appeared the minimum number of times before
+print(roster.items())       #Check if the database updates the student's number of visits
